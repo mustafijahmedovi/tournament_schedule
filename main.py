df596ad3f795fadf2c69b34b1c44eb947f5277ef
Mustafij Ahmed Ovi <mustafijahmed03@gmail.com>
2026-07-21 21:41:12 +0600

    ignore file set

--- main.py
+++ main.py
@@ -517,3 +517,10 @@ def set_knockout_schedule(input: KnockoutScheduleInput):
     conn.close()
 
     return {"message": "Knockout match schedule saved"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
